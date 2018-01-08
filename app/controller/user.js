9062b39962dddbc76d67c451024812a9274c5e45
linyongping <linyongping@outlook.com>
2018-01-08 18:16:44 +0800

    create and query data from database

--- app/controller/user.js
+++ app/controller/user.js
@@ -4,21 +4,20 @@ const Controller = require('egg').Controller;
 
 class UserController extends Controller {
   async query() {
-    const request = this.ctx.request;
-    console.log('request', request)
-    this.ctx.body = await this.ctx.model.User.find({username: 'sean'});
+    const { query } = this.ctx.request;
+    this.ctx.body = await this.ctx.model.User.find({ ...query });
   }
 
   async create() {
     const { ctx, service } = this;
     const params = ctx.request.body;
-    const isExist = await service.user.query(body.username)
-    if(isExist) {
-      const err = new Error('Username already exist')
-      err.status = 400;
-      throw err
-    }
-    const res = await service.user.create(params)
+    // const isExist = await this.ctx.model.User.find({ username: params.username })
+    // if(isExist) {
+    //   const err = new Error('Username already exist')
+    //   err.status = 400;
+    //   throw err
+    // }
+    const res = await this.ctx.model.User.create(params)
     ctx.body = { id: res.id }
     ctx.status = 201
   }
--- config/config.default.js
+++ config/config.default.js
@@ -1,18 +1,25 @@
-'use strict';
+"use strict";
 
 module.exports = appInfo => {
-  const config = exports = {};
+  const config = (exports = {});
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1515377406234_884';
+  config.keys = appInfo.name + "_1515377406234_884";
 
   // add your config here
   config.middleware = [];
 
   config.mongoose = {
-    url: 'mongodb://localhost:27017/my-blog',
-    options: { }
-  }
+    url: "mongodb://localhost:27017/my-blog",
+    options: {}
+  };
+
+  config.security = {
+    csrf: {
+      // 判断是否需要 ignore 的方法，请求上下文 context 作为第一个参数
+      enable: false
+    }
+  };
 
   return config;
 };
